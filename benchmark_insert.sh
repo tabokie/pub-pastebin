f753a4e420cdb67049f04914716fcc0678239f29
tabokie <xy.tao@outlook.com>
2023-07-27 13:09:18 +0200

    update sysbench
    Signed-off-by: tabokie <xy.tao@outlook.com>

---
+++ benchmark_insert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ueo pipefail
+
+source configurations.sh
+
+./private/stop.sh
+./private/restore_cluster.sh ${1}
+tiup cluster start ${TIUP_NAME}
+cd scripts
+
+./oltp_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=128 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t128.insert.run
+sleep 5m
+./oltp_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=512 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t512.insert.run
+
+cd ..
+./private/stop.sh
+./private/restore_cluster.sh ${1}
+tiup cluster start ${TIUP_NAME}
+cd scripts
+
+mysql -u root -h ${TIDB} -P 4000 -Bse "create database test1";
+mysql -u root -h ${TIDB} -P 4000 -Bse "create database test2";
+./bulk_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test1 --db-driver=mysql --report-interval=10 --threads=32 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t32.bulk_insert.run
+sleep 30m
+./bulk_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test2 --db-driver=mysql --report-interval=10 --threads=128 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t128.bulk_insert.run
+
--- benchmark_sysbench.sh
+++ benchmark_sysbench.sh
@@ -22,17 +22,28 @@ sleep 5m
 ./oltp_update_non_index.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=256 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t256.update_non_index.run
 sleep 5m
 ./oltp_update_non_index.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=512 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t512.update_non_index.run
-sleep 15m
+
+cd ..
+./private/stop.sh
+./private/restore_cluster.sh ${1}
+tiup cluster start ${TIUP_NAME}
+cd scripts
 
 ./oltp_update_index.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=128 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t128.update_index.run
 sleep 5m
 ./oltp_update_index.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=256 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t256.update_index.run
 sleep 5m
 ./oltp_update_index.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=512 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t512.update_index.run
-sleep 5m
+
+cd ..
+./private/stop.sh
+./private/restore_cluster.sh ${1}
+tiup cluster start ${TIUP_NAME}
+cd scripts
 
 ./oltp_read_write.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=128 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t128.read_write.run
 sleep 5m
 ./oltp_read_write.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=256 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t256.read_write.run
 sleep 5m
 ./oltp_read_write.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test --db-driver=mysql --report-interval=10 --threads=512 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t512.read_write.run
+
