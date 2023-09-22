32873f18983545fb80f47389af6f1d6de8a3639f
tabokie <xy.tao@outlook.com>
2023-09-22 09:01:02 +0200

    update
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- benchmark_insert.sh
+++ benchmark_insert.sh
@@ -20,6 +20,8 @@ cd scripts
 
 mysql -u root -h ${TIDB} -P 4000 -Bse "create database test1";
 mysql -u root -h ${TIDB} -P 4000 -Bse "create database test2";
+./bulk_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test1 --db-driver=mysql --report-interval=10 --threads=32 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} prepare | tee t32.bulk_insert.prepare
+./bulk_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test2 --db-driver=mysql --report-interval=10 --threads=128 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} prepare | tee t128.bulk_insert.prepare
 ./bulk_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test1 --db-driver=mysql --report-interval=10 --threads=32 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t32.bulk_insert.run
 sleep 30m
 ./bulk_insert.lua --mysql-host=${TIDB} --mysql-port=4000 --mysql-user=root --mysql-db=test2 --db-driver=mysql --report-interval=10 --threads=128 --table_size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLE_COUNT} --time=${SYSBENCH_DURATION} run | tee t128.bulk_insert.run
